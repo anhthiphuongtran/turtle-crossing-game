1952892267db4b243bc370e81fb688f4e0b62522
Anh Tran <105230494+anhthiphuongtran@users.noreply.github.com>
2022-07-01 09:17:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ car_manager = CarManager()
 scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(player.go_up, "Up")
+screen.onkeypress(player.go_up, "Up")
 
 game_is_on = True
 while game_is_on:
